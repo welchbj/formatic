80132286a7966ffdd05331c2684e952f2a9e8441
Brian Welch <welch18@vt.edu>
2019-09-22 16:05:01 -0400

    Add code obj injection walkers to module init

--- formatic/walkers/__init__.py
+++ formatic/walkers/__init__.py
@@ -2,6 +2,10 @@ from .abstract_injection_walker import (  # noqa
     AbstractInjectionWalker)
 from .class_injection_walker import (  # noqa
     ClassInjectionWalker)
+from .code_object_injection_walker import (  # noqa
+    CodeObjectInjectionWalker)
+from .code_object_field_injection_walker import (  # noqa
+    CodeObjectFieldInjectionWalker)
 from .function_injection_walker import (  # noqa
     FunctionInjectionWalker)
 from .slot_wrapper_injection_walker import ( # noqa
